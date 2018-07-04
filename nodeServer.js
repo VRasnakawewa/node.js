1bb5a3bd8a3064c81a7ba3555a9cf1ee03c12c0b
VRasnakawewa <vihangarasnakawewa1@gmail.com>
2018-07-04 13:53:48 +0530

    add nodemon

--- nodeServer.js
+++ nodeServer.js
@@ -1,5 +1,5 @@
 var http = require("http");
 http.createServer(function(req,res){
-    res.end("Hello, world!");
-    console.log("Server is listening");
+    res.end("Hello, world! Nodemon");
 }).listen(8080);
+console.log("Server is listening");
